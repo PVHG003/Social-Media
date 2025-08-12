630084a759fff39c09ca90d351cf34ab809f55e4
PVHG <phamhung155tp@gmail.com>
2025-08-12 14:56:58 +0700

    feat: Added backend folder structure

--- backend/src/main/java/vn/pvhg/backend/BackendApplication.java
+++ backend/src/main/java/vn/pvhg/backend/BackendApplication.java
@@ -3,11 +3,9 @@ package vn.pvhg.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import vn.pvhg.backend.properties.JwtKeyProperties;
-import vn.pvhg.backend.properties.UploadDirProperties;
 
 @SpringBootApplication
-@EnableConfigurationProperties({JwtKeyProperties.class, UploadDirProperties.class})
+@EnableConfigurationProperties()
 public class BackendApplication {
 
     public static void main(String[] args) {
