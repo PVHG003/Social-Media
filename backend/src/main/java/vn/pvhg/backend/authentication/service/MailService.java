e543b8105ea80fbe6884d4f14eee3949ebd7c8f6
PVHG003 <phamhung155tp@gmail.com>
2025-08-20 20:15:18 +0700

    ""

--- backend/src/main/java/vn/pvhg/backend/authentication/service/MailService.java
+++
@@ -1,5 +0,0 @@
-package vn.pvhg.backend.authentication.service;
-
-public interface MailService {
-    void sendOtpEmail(String toEmail, String otpCode);
-}
--- backend/src/main/java/vn/pvhg/backend/authentication/service/VerifyUserService.java
+++
@@ -1,6 +0,0 @@
-package vn.pvhg.backend.authentication.service;
-
-public interface VerifyUserService {
-    String generateAndSaveOtp(Long userId);
-    boolean verifyOtp(Long userId, String verificationCode);
-}
