a0f39d8114f1142a5cac996a8ef5e101901d0153
PVHG003 <phamhung155tp@gmail.com>
2025-08-22 18:23:00 +0700

    refactor: enhance type definitions and improve function signatures in chat API

--- frontend/src/services/chat/apiChat.ts
+++ frontend/src/services/chat/apiChat.ts
@@ -2,12 +2,15 @@ import {
   ChatControllerApi,
   Configuration,
   type AddMembersRequest,
+  type ApiPaginatedResponseListChatListResponse,
+  type ApiPaginatedResponseListChatMessageResponse,
+  type ApiResponseChatDetailResponse,
+  type ApiResponseVoid,
   type ChatCreateRequest,
   type Pageable,
 } from "@/api";
 
 const configuration = new Configuration({
-  basePath: "http://localhost:8080",
   accessToken: `Bearer ${localStorage.getItem("token")}`,
   baseOptions: {
     withCredentials: true,
@@ -18,56 +21,74 @@ const configuration = new Configuration({
 const chatControllerApi = new ChatControllerApi(configuration);
 
 const chatApi = {
-  getChatInfo: async (chatId: string) => {
+  getChatInfo: async (
+    chatId: string
+  ): Promise<ApiResponseChatDetailResponse> => {
     const { data } = await chatControllerApi.getChatInfo(chatId);
     if (!data.success) {
       throw new Error();
     }
     return data;
   },
-  updateChat: async (chatId: string, request: ChatCreateRequest) => {
+  updateChat: async (
+    chatId: string,
+    request: ChatCreateRequest
+  ): Promise<ApiResponseChatDetailResponse> => {
     const { data } = await chatControllerApi.updateChat(chatId, request);
     if (!data.success) {
       throw new Error(data.message);
     }
     return data;
   },
-  deleteChat: async (chatId: string) => {
+  deleteChat: async (chatId: string): Promise<ApiResponseVoid> => {
     const { data } = await chatControllerApi.deleteChat(chatId);
     if (!data.success) {
       throw new Error(data.message);
     }
     return data;
   },
-  getChatList: async (pageable: Pageable) => {
+  getChatList: async (
+    pageable: Pageable
+  ): Promise<ApiPaginatedResponseListChatListResponse> => {
     const { data } = await chatControllerApi.getChatList(pageable);
     if (!data.success) {
       throw new Error(data.message);
     }
     return data;
   },
-  createChat: async (request: ChatCreateRequest) => {
+  createChat: async (
+    request: ChatCreateRequest
+  ): Promise<ApiResponseChatDetailResponse> => {
     const { data } = await chatControllerApi.createChat(request);
     if (!data.success) {
       throw new Error(data.message);
     }
     return data;
   },
-  addMembers: async (chatId: string, request: AddMembersRequest) => {
+  addMembers: async (
+    chatId: string,
+    request: AddMembersRequest
+  ): Promise<ApiResponseChatDetailResponse> => {
     const { data } = await chatControllerApi.addMembers(chatId, request);
     if (!data.success) {
       throw new Error(data.message);
     }
     return data;
   },
-  getChatMessages: async (chatId: string, pageable: Pageable) => {
+  getChatMessages: async (
+    chatId: string,
+    pageable: Pageable
+  ): Promise<ApiPaginatedResponseListChatMessageResponse> => {
     const { data } = await chatControllerApi.getChatMessages(chatId, pageable);
     if (!data.success) {
       throw new Error(data.message);
     }
     return data;
   },
-  removeMember: async (chatId: string, userId: string) => {
+  removeMember: async (
+    chatId: string,
+    userId: string
+  ): Promise<ApiResponseChatDetailResponse> => {
     const { data } = await chatControllerApi.removeMember(chatId, userId);
     if (!data.success) {
       throw new Error(data.message);
